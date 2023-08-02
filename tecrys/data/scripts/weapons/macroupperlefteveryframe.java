2fb172ea23841088536d5d17768f2c8314faee94
p2016p <cohenmatthew99@gmail.com>
2023-08-01 22:19:06 -0400

    more advanced articulation

--- tecrys/data/scripts/weapons/macroupperlefteveryframe.java
+++ tecrys/data/scripts/weapons/macroupperlefteveryframe.java
@@ -1,7 +1,9 @@
 package tecrys.data.scripts.weapons;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.util.IntervalUtil;
+import org.lazywizard.lazylib.MathUtils;
 import org.lazywizard.lazylib.VectorUtils;
 import org.lwjgl.util.vector.Vector2f;
 
@@ -10,7 +12,9 @@ import java.awt.*;
 public class macroupperlefteveryframe implements EveryFrameWeaponEffectPlugin {
 
     float index = 0f;
-    IntervalUtil interval = new IntervalUtil(0.02f, 0.02f);
+    IntervalUtil interval = new IntervalUtil(0.01f, 0.01f);
+
+    private static org.apache.log4j.Logger log = Global.getLogger(macroupperlefteveryframe.class);
 
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
@@ -43,11 +47,28 @@ public class macroupperlefteveryframe implements EveryFrameWeaponEffectPlugin {
         interval.advance(amount);
 
         if(interval.intervalElapsed()) {
+            Vector2f targloc = ship.getMouseTarget();
             float offset = 1f;
             if (arm != null) {
                 //float offset = 0f;
 
-                arm.setCurrAngle(arm.getCurrAngle() + offset);
+                Vector2f slotabsloc = VectorUtils.rotateAroundPivot(new Vector2f(arm.getSlot().getLocation().getX()+ship.getLocation().getX(), arm.getSlot().getLocation().getY()+ship.getLocation().getY()),ship.getLocation(), ship.getFacing());
+                float dist = MathUtils.getDistance(slotabsloc,targloc)*0.4f;
+
+                float targangle = (float) (Math.asin((MathUtils.clamp(dist,0f,106f)/106f)))*57.2958f;
+
+                log.info(dist);
+                log.info(targangle);
+
+                float dangle = (3600f+MathUtils.clamp(VectorUtils.getAngle(slotabsloc,targloc)+90f - targangle,-10f+ship.getFacing(), 110f+ship.getFacing()))%360f;
+                float currangle =  (3600f+arm.getCurrAngle())%360f;
+
+                if(dangle > currangle+3f){
+                    arm.setCurrAngle(currangle+3f);
+                }else if(dangle < currangle-3f){
+                    arm.setCurrAngle(currangle-3f);
+                }
+
             }
 
             if (forearm != null) {
@@ -55,7 +76,19 @@ public class macroupperlefteveryframe implements EveryFrameWeaponEffectPlugin {
 
                 //float offset = 0f;
 
-                forearm.setCurrAngle(forearm.getCurrAngle()+2*offset);
+                Vector2f slotabsloc = VectorUtils.rotateAroundPivot(new Vector2f(arm.getSlot().getLocation().getX()+ship.getLocation().getX(), arm.getSlot().getLocation().getY()+ship.getLocation().getY()),ship.getLocation(), ship.getFacing());
+                float dist = MathUtils.getDistance(slotabsloc,targloc)*0.6f;
+
+                float targangle = (float) (Math.asin((MathUtils.clamp(dist,0f,119f)/119f)))*57.2958f;
+
+                float dangle = (3600f+VectorUtils.getAngle(slotabsloc,targloc)+90f - 180f + targangle)%360f;
+                float currangle =  (3600f+forearm.getCurrAngle())%360f;
+
+                if(dangle > currangle+3f){
+                    forearm.setCurrAngle(currangle+3f);
+                }else if(dangle < currangle-3f){
+                    forearm.setCurrAngle(currangle-3f);
+                }
             }
 
             if (hand != null) {
@@ -63,7 +96,7 @@ public class macroupperlefteveryframe implements EveryFrameWeaponEffectPlugin {
 
                 //float offset = 0f;
 
-                hand.setCurrAngle(hand.getCurrAngle()+3*offset);
+                hand.setCurrAngle(forearm.getCurrAngle());
             }
         }
     }
